45ef4c7a695032e05aec8a27b4109a0ad43a5e33
PaulLai1022 <71100835+PaulLai1022@users.noreply.github.com>
2024-11-16 17:46:02 -0800

    push

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let menu = document.querySelector('#menu-bars');
 let header = document.querySelector('header');
 
+let card1 = document.getElementById("card1");
+
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     header.classList.toggle('active');
@@ -10,3 +12,27 @@ menu.onscroll = () =>{
     menu.classList.remove('fa-times');
     header.classList.remove('active');
 }
+
+
+
+window.addEventListener("scroll",()=>{
+    const viewPortHeight = window.innerHeight;
+ 
+    const cardPosition = card1.getBoundingClientRect();
+
+    // console.log(cardPosition);
+    
+    // console.log(`test:${cardPosition.top+cardPosition.height/2}`);
+    // console.log("windows height: " + window.innerHeight);
+    // console.log("cardBotton:" + cardPosition.bottom);
+
+    if(cardPosition.top+cardPosition.height/2 < viewPortHeight && cardPosition.bottom > 0){
+        console.log("windows height: " + window.innerHeight);
+        console.log(cardPosition);
+        
+        console.log("cardTop"+cardPosition.top);
+        console.log("cardBotton:" + cardPosition.bottom);
+    }
+   
+
+})
